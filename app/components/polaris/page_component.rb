0bb31326167d580d6bb0439f25673d1468425db6
Kirill Platonov <kirill@platonov.net>
2022-04-09 09:37:29 +0300

    Finalize Page component

--- app/components/polaris/page_component.rb
+++ app/components/polaris/page_component.rb
@@ -5,14 +5,17 @@ module Polaris
     renders_one :primary_action, ->(primary: true, **system_arguments) do
       Polaris::ButtonComponent.new(primary: primary, **system_arguments)
     end
-    # renders_many :secondary_actions, Polaris::ButtonComponent
+    renders_many :secondary_actions, Polaris::ButtonComponent
     renders_one :title_metadata
     renders_one :thumbnail, Polaris::ThumbnailComponent
 
     def initialize(
       title: nil,
       subtitle: nil,
+      compact_title: false,
       back_url: nil,
+      prev_url: nil,
+      next_url: nil,
       narrow_width: false,
       full_width: false,
       divider: false,
@@ -20,7 +23,10 @@ module Polaris
     )
       @title = title
       @subtitle = subtitle
+      @compact_title = compact_title
       @back_url = back_url
+      @prev_url = prev_url
+      @next_url = next_url
 
       @system_arguments = system_arguments
       @system_arguments[:tag] = "div"
@@ -49,8 +55,22 @@ module Polaris
       )
     end
 
+    def subtitle_arguments
+      {
+        tag: "div",
+        classes: class_names(
+          "Polaris-Header-Title__SubTitle",
+          "Polaris-Header-Title__SubtitleCompact": @compact_title
+        )
+      }
+    end
+
     def render_header?
       @title.present? || @subtitle.present? || @back_url.present? || primary_action.present?
+    end
+
+    def has_pagination?
+      @next_url.present? || @prev_url.present?
     end
   end
 end
